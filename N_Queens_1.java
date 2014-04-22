94c324948eee5af3cb3bcd8d21721ffe2d0ac079
Yizhe Liu <wxliuyizhe@gmail.com>
2014-04-21 23:14:18 -0700

    Update N_Queens_1.java

--- N_Queens_1.java
+++ N_Queens_1.java
@@ -1,10 +1,12 @@
 public class Solution {
-    // public static Integer lastPostion=null;
     public ArrayList<String[]> solveNQueens(int n) {
         if(n <= 0) return null;
         ArrayList<String[]> result = new ArrayList<String[]>();
-        
+
+        //config is used to store reasonable configurations. The i th element in config stores the position of one           //queen at i th row, i.e. if config.get(1) is 2, then it means there is one queen at row 1, column 2
+
         ArrayList<Integer> config = new ArrayList<Integer>();
+
         for(int i = 0; i< n; i++){
             
             config.clear();
@@ -18,7 +20,7 @@ public class Solution {
     }
     
     public void findConfig(ArrayList<Integer> config, int n, ArrayList<String[]> result){
-        if(config.size() == n) {
+        if(config.size() == n) // if the config has n elements, it means this config is reasonable
             result.add(convert2StrArr(config));
             config.remove(n - 1);
             return;
@@ -32,12 +34,12 @@ public class Solution {
         config.remove(config.size() - 1);
     }
     
-    public boolean canBeInBoard(ArrayList<Integer> config,int idx){
+    public boolean canBeInBoard(ArrayList<Integer> config,int idx){// find reasonable position for next row
         int len = config.size();
         for(int i = 0; i < len; i++){
             if(config.get(i) == idx) return false;// can not be at the same column
-            if(Math.abs(i - len) == Math.abs(config.get(i) - idx)) return false;
-            
+            if(Math.abs(i - len) == Math.abs(config.get(i) - idx))
+               return false;// can not be at the same diagonal.
         }
         return true;
     }
